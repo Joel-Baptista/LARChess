076ad9be2d0950ee93c89fe2409c3b5adba72b06
Joel-Baptista <joelbaptista@ua.pt>
2024-08-06 13:15:37 +0100

    There's progress in controlling the windows aspect ratio and reading the mouse inputs simultaneously

--- src/minmax.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "board.h"
-
-std::tuple<std::string, double> minmax(Board board, int depth, bool maximizing_player, double alpha, double beta);
-double evaluate(Board& board);
-double piece_value(int piece);
