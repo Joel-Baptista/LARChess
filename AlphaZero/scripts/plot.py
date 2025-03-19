f2ee640b946741d91ccad3007d1229520ec650a3
Joel Baptista <joelbaptista@ua.pt>
2025-03-19 20:14:46 +0000

    minor changes in config filres

--- AlphaZero/scripts/plot.py
+++ AlphaZero/scripts/plot.py
@@ -7,7 +7,7 @@ import os
 sns.set_theme(style="whitegrid")
 
 # Folder containing your CSV files
-folder_path = '../models/slearn/model22'
+folder_path = '../models/slearn/model21'
 # folder_path = '../models/alpha/model28'
 
 # Iterate through each CSV file in the folder
